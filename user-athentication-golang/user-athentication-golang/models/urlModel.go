3b74f58c52b8d5bb92a8db706c0c3c1093f91178
taraneh-bhr <72089592+taraneh-bhr@users.noreply.github.com>
2022-07-01 02:34:27 +0430

    Delete user-athentication-golang/user-athentication-golang directory

--- user-athentication-golang/user-athentication-golang/models/urlModel.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type URL struct {
-	URL       string `json:"url" validate:"required""`
-	Threshold int    `json:"threshold" validate:"required,min=1,max=2""`
-	Failed    int    `json:"failed"`
-	Succeed   int    `json:"succeed"`
-}
