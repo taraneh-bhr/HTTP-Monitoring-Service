1743073551118791131959f9ae64840186215f7c
taraneh-bhr <72089592+taraneh-bhr@users.noreply.github.com>
2022-07-01 02:49:00 +0430

    Add files via upload

--- user-athentication-golang/routes/userRouter.go
+++ user-athentication-golang/routes/userRouter.go
@@ -2,6 +2,7 @@ package routes
 
 import (
     controller "user-athentication-golang/controllers"
+    middleware "httpmon.com/first/middleware"
 
     "github.com/gin-gonic/gin"
 )
@@ -10,4 +11,7 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine) {
     incomingRoutes.POST("/users/signup", controller.SignUp())
     incomingRoutes.POST("/users/login", controller.Login())
+    incomingRoutes.Use(middleware.Authentication())
+	incomingRoutes.GET("/users/history", controller.GetHistory())
+	incomingRoutes.GET("/users/alerts", controller.GetAlerts())
 }
